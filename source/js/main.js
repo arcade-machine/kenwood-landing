37d896d395e7dd10380d05e36c1bcb37abc1815b
Max <tool13@mail.ru>
2020-12-05 20:40:46 +0300

    finish page

--- source/js/main.js
+++ source/js/main.js
@@ -3,4 +3,16 @@ const burger = header.querySelector('.header__burger');
 
 burger.addEventListener('click', () => {
     header.classList.toggle('header--open');
-})
+});
+
+$('.characteristics-open').each(function() {
+    $(this).on('click', function() {
+        $(this).closest('.toward-view__info').find('.toward-view__characteristics').addClass('toward-view__characteristics--visible');
+    });
+});
+
+$('.toward-view__characteristics-close').each(function() {
+    $(this).on('click', function() {
+        $(this).closest('.toward-view__characteristics--visible').removeClass('toward-view__characteristics--visible');
+    });
+});
