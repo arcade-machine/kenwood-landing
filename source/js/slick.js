3b8bdd2fae599036b5f7e81140a1c35df989ea5c
Max <tool13@mail.ru>
2020-12-05 16:23:08 +0300

    add few blocks

--- source/js/slick.js
+++ source/js/slick.js
@@ -47,3 +47,25 @@ $('.features__slider').slick({
     ]
 });
 
+$('.features__slider').slick({
+    dots: true,
+    arrows: true,
+    autoplay: false,
+    infinite: false,
+    responsive: [
+        {
+            breakpoint: 9999,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }
+    ]
+});
+
