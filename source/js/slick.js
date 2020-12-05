5a54db4fd61777d48c801e8a75c2e9a556ff2dd1
Max <tool13@mail.ru>
2020-12-05 16:55:30 +0300

    add toward view

--- source/js/slick.js
+++ source/js/slick.js
@@ -47,25 +47,11 @@ $('.features__slider').slick({
     ]
 });
 
-$('.features__slider').slick({
-    dots: true,
-    arrows: true,
-    autoplay: false,
-    infinite: false,
-    responsive: [
-        {
-            breakpoint: 9999,
-            settings: "unslick"
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        }
-    ]
+$('.toward-view__slider').each(function() {
+    $(this).slick({
+        arrows: false,
+        dots: true,
+        autoplay: false,
+        infinite: false,
+    });
 });
-
